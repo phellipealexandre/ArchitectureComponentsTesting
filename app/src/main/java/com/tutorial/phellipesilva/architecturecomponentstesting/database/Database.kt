e31cc56c8dc32a0cd1210342c4ae44f98af14129
Phellipe Silva <pafsilva@thoughtworks.com>
2018-07-14 22:51:18 -0300

    Clear unused imports

--- app/src/main/java/com/tutorial/phellipesilva/architecturecomponentstesting/database/Database.kt
+++ app/src/main/java/com/tutorial/phellipesilva/architecturecomponentstesting/database/Database.kt
@@ -2,7 +2,6 @@ package com.tutorial.phellipesilva.architecturecomponentstesting.database
 
 import android.arch.persistence.room.Database
 import android.arch.persistence.room.RoomDatabase
-import kotlin.reflect.KClass
 
 @Database(entities = [(User::class)], version = 1)
 abstract class Database : RoomDatabase() {
